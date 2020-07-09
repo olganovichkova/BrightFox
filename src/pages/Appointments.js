74e2b2fcd03b9bd17450d6a2ca11e974f8e6381b
Olga Novichkova <olganovichkova3@gmail.com>
2020-07-08 22:41:45 -0600

    filtered/grouped start time and added icons

--- src/pages/Appointments.js
+++ src/pages/Appointments.js
@@ -3,9 +3,33 @@ import { Typography, Badge } from "@material-ui/core";
 import axios from "axios";
 import { withAuth } from "@okta/okta-react";
 import moment from "moment";
+import { FaClock, FaMapMarkerAlt } from "react-icons/fa";
+import { groupBy } from "lodash";
 
 import CurrentTime from "../components/CurrentTime";
 const API = process.env.REACT_APP_API || "http://localhost:3001";
+const getStartHour = (appt) => {
+  //   {
+  //     "id":3292034,
+  //     "start":"2020-07-08T23:30:00Z",
+  //     "finish":"2020-07-09T01:00:00Z",
+  //     "topic":"SAT Math",
+  //     "status":"planned",
+  //     "service":{
+  //        "id":150075,
+  //        "name":"Math / Science",
+  //        "dft_charge_type":"hourly",
+  //        "created":"2018-12-11T22:20:28.200674Z",
+  //        "dft_charge_rate":"65.00",
+  //        "dft_contractor_rate":"0.50",
+  //        "status":"in-progress",
+  //        "url":"https://secure.tutorcruncher.com/api/services/150075/"
+  //     },
+  //     "url":"https://secure.tutorcruncher.com/api/appointments/3292034/"
+  //  }
+  console.log(moment(appt.start).format("h"));
+  return moment(appt.start).format("h");
+};
 
 export default withAuth((props) => {
   const [init, updateInit] = useState(false);
@@ -13,7 +37,9 @@ export default withAuth((props) => {
   const [allAppts, updateAllAppts] = useState([]);
   const [totalCount, updateTotalCount] = useState(0);
   const [nextUrl, updateNextUrl] = useState(
-    "/appointments?start_gte=2020-06-18&start_lte=2020-06-19"
+    `/appointments?start_gte=${moment().format(
+      "YYYY-MM-DD"
+    )}&start_lte=${moment().add(1, "days").format("YYYY-MM-DD")}`
   );
   const [requestCount, updateRequestCount] = useState(0);
   const [startTime, updateStartTime] = useState(moment().format("h:mm"));
@@ -35,6 +61,8 @@ export default withAuth((props) => {
         .then((response) => {
           updateInit(true);
           console.log(response);
+          let group = groupBy(response.data.results, getStartHour);
+          console.log(group);
           let responseNextUrl = response.data.next;
           if (responseNextUrl != null) {
             let index = responseNextUrl.indexOf("/api/") + 4;
@@ -56,7 +84,9 @@ export default withAuth((props) => {
           updateAppointments(response.data.results);
         });
     }
-    fetchData();
+    if (token) {
+      fetchData();
+    }
   }, [token]);
 
   useEffect(() => {
@@ -97,12 +127,24 @@ export default withAuth((props) => {
       <div></div>
       <div className="container-fluid">
         <div className="row">
-          <div className="col-sm-6"></div>
-          <div className="col-sm-6">
+          <div className="col-sm-4"></div>
+          <div className="col-sm-4"></div>
+          <div className="col-sm-4">
             <CurrentTime />
           </div>
         </div>
         <br />
+        <div className="row">
+          <div className="col-sm-4"></div>
+          <div className="col-sm-4">
+            <h1>
+              <FaMapMarkerAlt />
+            </h1>
+          </div>
+          <div className="col-sm-4"></div>
+        </div>
+        <br />
+
         <div className="row no-gutters row-card-wrapper">
           {appointments.map((appointment) => {
             var start = moment(appointment.start).format("h:mm");
@@ -111,6 +153,12 @@ export default withAuth((props) => {
                 <div className="card person-card appt-card">
                   <div className="card-body">
                     <h1>{start}</h1>
+                    <h1>
+                      <FaClock />
+                    </h1>
+                    <h1>
+                      <FaMapMarkerAlt />
+                    </h1>
                     <h4>{appointment.topic}</h4>
                     <h2>{appointment.location}</h2>
                   </div>
