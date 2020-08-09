84ab2ba82fa3d906f47510ef7ae2e18ffe785937
Elena Novichkova <elenanovichkova@Elenas-MacBook-Pro.local>
2020-08-08 17:33:45 -0700

    test deploy 1

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from "./App";
 import "./styles/style.css";
 
 const oktaConfig = {
-  issuer: `${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`,
+  issuer: `https://dev-109690.okta.com/oauth2/default`,
   redirect_uri: `${window.location.origin}/implicit/callback`,
   client_id: process.env.REACT_APP_OKTA_CLIENT_ID,
 };
