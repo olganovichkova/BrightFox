220b3a68a45e56b964b0ab580f36feb8ec171697
Olga Novichkova <olganovichkova3@gmail.com>
2020-09-01 22:44:43 -0600

    clean up code and fixed location

--- src/components/RoomNumber.js
+++ src/components/RoomNumber.js
@@ -4,7 +4,7 @@ const RoomNumber = (props) => {
   if (!props.location) {
     return <span className="location-time-font"> - </span>;
   }
-  return <span className="location-time-font">Room {props.location}</span>;
+  return <span className="location-time-font">{props.location.name}</span>;
 };
 
 export default RoomNumber;
